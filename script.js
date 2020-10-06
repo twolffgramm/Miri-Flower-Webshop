86c23bb23a99ad7f4366896c72f74f3e233f90eb
twolffgramm <69900545+twolffgramm@users.noreply.github.com>
2020-10-06 20:57:17 +0200

    nav-update & card width test

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+// get Elements
 var navbar = document.getElementById("navbar");
 var homesection = document.getElementById("home-section");
 var bandleft = document.getElementById("bandleft");
@@ -8,46 +8,47 @@ var downarrow = document.getElementById("downarrow");
 var mobilenav = document.getElementById("mobile-nav");
 var hamburger = document.getElementById("hamburger");
 
-const scrolled = "nav-container-scrolled";
-const homescrolled = "home-scrolled";
-const home = "home";
-const moveinleft = "bandleft-movedin";
-const moveinright = "bandright-movedin";
-const moveinupper = "bandupper-movedin";
-const removearrow = "downarrow-scrolled";
-const mobilenavactive = "mobile-nav-active";
-
-
+// Animations on specific scroll position
 window.addEventListener("scroll", () => {
-
   const currentScroll = window.pageYOffset;
-
-  if (currentScroll >= 300 && !bandleft.classList.contains(moveinleft)){
-    bandleft.classList.add(moveinleft);
-    bandright.classList.add(moveinright);
-    bandupper.classList.add(moveinupper);
+  if (currentScroll >= 300 && !bandleft.classList.contains("bandleft-movedin")){
+    bandleft.classList.add("bandleft-movedin");
+    bandright.classList.add("bandright-movedin");
+    bandupper.classList.add("bandupper-movedin");
   }
-  if (currentScroll >= 500 && !navbar.classList.contains(scrolled)){
-    navbar.classList.add(scrolled);
-    downarrow.classList.add(removearrow);
+  if (currentScroll >= 500 && !navbar.classList.contains("nav-container-scrolled")){
+    navbar.classList.add("nav-container-scrolled");
+    downarrow.classList.add("downarrow-scrolled");
   }
-  if (currentScroll >= 700 && !homesection.classList.contains(homescrolled)){
-    homesection.classList.remove(home)
-    homesection.classList.add(homescrolled); 
+  if (currentScroll >= 700 && !homesection.classList.contains("home-scrolled")){
+    homesection.classList.remove("home")
+    homesection.classList.add("home-scrolled"); 
   }
-  
-  console.log(currentScroll);
 });
 
-
+// Arrowclick
 downarrow.addEventListener("click", () => {
   window.scrollTo(0, 700);
   downarrow.remove();
 })
 
+// mobile menu
 hamburger.addEventListener("click", () => {
-  mobilenav.classList.toggle(mobilenavactive);
+  mobilenav.classList.toggle("mobile-nav-active");
   hamburger.classList.toggle("hamburger-toggle");
 })
 
+// collapse mobile menu on click outside navbar
+document.body.addEventListener("click",function(e) {
+  if (!e.target.classList.contains("navbar") && 
+  e.target.id != "logo" && 
+  !e.target.classList.contains("line1") && 
+  !e.target.classList.contains("line2") && 
+  !e.target.classList.contains("line3") && 
+  !e.target.classList.contains("hamburger") && 
+  hamburger.classList.contains("hamburger-toggle")){
+    mobilenav.classList.remove("mobile-nav-active");
+    hamburger.classList.remove("hamburger-toggle");
+  }
+});
 
