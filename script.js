8d84f0761c84cd832c41288ba259a65fbdc7b306
twolffgramm <69900545+twolffgramm@users.noreply.github.com>
2020-10-07 15:36:14 +0200

    typewriter

--- script.js
+++ script.js
@@ -52,3 +52,58 @@ document.body.addEventListener("click",function(e) {
   }
 });
 
+// Typewriter effect
+var TxtType = function(el, toRotate, period, font) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.font = font;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+  var font = this.font[i];
+ 
+  if (this.isDeleting) {
+  this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+  this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap" style="font-family:'+font+'">'+this.txt+'</span>';
+
+  var that = this;
+  var delta = 250 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+  delta = this.period;
+  this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+  this.isDeleting = false;
+  this.loopNum++;
+  delta = 500;
+  }
+
+  setTimeout(function() {
+  that.tick();
+  }, delta);
+};
+
+window.onload = function() {
+  var elements = document.getElementsByClassName('typewrite');
+  for (var i=0; i<elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-type');
+      var period = elements[i].getAttribute('data-period');
+      var font = elements[i].getAttribute('font-type');
+      if (toRotate) {
+        new TxtType(elements[i], JSON.parse(toRotate), period,JSON.parse(font));
+      }
+  }
+};
